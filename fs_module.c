53caaf6c9ed05c6e6de126171545edfcd2de3f9f
Artem Matson <supermatson42@gmail.com>
2023-12-13 00:43:11 +0300

    done

--- fs_module.c
+++ fs_module.c
@@ -1,28 +1,38 @@
-#include <linux/fs.h>
 #include <linux/fs_context.h>
-#include <linux/init.h>
-#include <linux/kernel.h>
 #include <linux/module.h>
 
+#include "fs_defs.h"
 #include "http.h"
 #include "models.h"
+
+#define ALLOC_INO                                           \
+  char *ino_ascii = kmalloc(sizeof(ino_t) + 1, GFP_KERNEL); \
+  if (ino_ascii == NULL) {                                  \
+    ret = -ENOMEM;                                          \
+    goto ino_end;                                           \
+  }
+
+#define FREE_INO    \
+  kfree(ino_ascii); \
+  ino_end:
+
+#define ALLOC_BUF(model)                            \
+  size_t buffer_size = sizeof(model);               \
+  model *buffer = kmalloc(buffer_size, GFP_KERNEL); \
+  if (buffer == NULL) {                             \
+    ret = -ENOMEM;                                  \
+    goto buf_end;                                   \
+  }
+
+#define FREE_BUF \
+  kfree(buffer); \
+  buf_end:
 
 #define TOKEN_PATTERN "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
 
 #define MAX_TITLE_LEN 255
 
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Matson Artem");
-MODULE_VERSION("0.01");
-
-struct inode *networkfs_get_inode(struct super_block *sb,
-                                  const struct inode *parent, umode_t mode,
-                                  int i_ino);
-
-int check_name_len(const char* name) {
-  return strlen(name) > MAX_TITLE_LEN;
-}
-
+int check_name_len(const char *name) { return strlen(name) > MAX_TITLE_LEN; }
 
 struct dentry *networkfs_lookup(struct inode *parent, struct dentry *child,
                                 unsigned int flag) {
@@ -130,12 +140,6 @@ int networkfs_mkdir(struct user_namespace *user_ns, struct inode *parent,
                                "directory");
 }
 
-struct inode_operations networkfs_inode_ops = {.lookup = &networkfs_lookup,
-                                               .create = &networkfs_create,
-                                               .unlink = &networkfs_unlink,
-                                               .mkdir = &networkfs_mkdir,
-                                               .rmdir = &networkfs_rmdir};
-
 int networkfs_iterate(struct file *filp, struct dir_context *ctx) {
   struct dentry *dentry = filp->f_path.dentry;
   struct inode *inode = dentry->d_inode;
@@ -175,10 +179,6 @@ free:
   FREE_BUF
   return ret;
 }
-
-struct file_operations networkfs_dir_ops = {
-    .iterate = &networkfs_iterate,
-};
 
 struct inode *networkfs_get_inode(struct super_block *sb,
                                   const struct inode *parent, umode_t mode,
@@ -223,9 +223,6 @@ int networkfs_get_tree(struct fs_context *fc) {
   return ret;
 }
 
-struct fs_context_operations networkfs_context_ops = {.get_tree =
-                                                          &networkfs_get_tree};
-
 int networkfs_init_fs_context(struct fs_context *fc) {
   fc->ops = &networkfs_context_ops;
   return 0;
@@ -237,10 +234,9 @@ void networkfs_kill_sb(struct super_block *sb) {
   printk(KERN_INFO "networkfs: superblock is destroyed");
 }
 
-struct file_system_type networkfs_fs_type = {
-    .name = "networkfs",
-    .init_fs_context = &networkfs_init_fs_context,
-    .kill_sb = &networkfs_kill_sb};
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Matson Artem");
+MODULE_VERSION("0.01");
 
 int networkfs_init(void) {
   int ret = register_filesystem(&networkfs_fs_type);
--- models.h
+++ models.h
@@ -1,5 +1,3 @@
-#include <linux/types.h>
-
 struct entry {
   unsigned char entry_type;  // DT_DIR (4) or DT_REG (8)
   ino_t ino;
@@ -19,26 +17,3 @@ struct entry_info {
 struct create_info {
   ino_t ino;
 };
-
-#define ALLOC_INO                                           \
-  char *ino_ascii = kmalloc(sizeof(ino_t) + 1, GFP_KERNEL); \
-  if (ino_ascii == NULL) {                                  \
-    ret = -ENOMEM;                                          \
-    goto ino_end;                                           \
-  }
-
-#define FREE_INO    \
-  kfree(ino_ascii); \
-  ino_end:
-
-#define ALLOC_BUF(model)                            \
-  size_t buffer_size = sizeof(model);               \
-  model *buffer = kmalloc(buffer_size, GFP_KERNEL); \
-  if (buffer == NULL) {                             \
-    ret = -ENOMEM;                                  \
-    goto buf_end;                                   \
-  }
-
-#define FREE_BUF \
-  kfree(buffer); \
-  buf_end:
